d2f2dcd89a73224abe5a6a1bd4945a39521bf693
izabellaT <izabelatodorova84@gmail.com>
2023-02-15 08:43:50 +0200

    masivi

---
+++ Masivi/Masivi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Masivi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                switch (command[0])
+                {
+                    case "Reverse":
+                        Array.Reverse(arr);
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(command[1]);
+                        string newWord = command[2];
+                        arr[index] = newWord;
+                        break;
+                    case "Print":
+                        {
+                            foreach (var item in arr)
+                            {
+                                if (item.StartsWith(command[1]))
+                                {
+                                    Console.Write(item + " ");
+                                }
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
+                }
+                Console.WriteLine(string.Join(", ", arr));
+            }
+        }
+    }
+}
