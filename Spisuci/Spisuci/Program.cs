1408c1722a92b24dab85d9a9ec9880fc0b62f595
izabellaT <izabelatodorova84@gmail.com>
2023-02-15 09:30:50 +0200

    zad1

---
+++ Spisuci/Spisuci/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int last = nums[nums.Count - 1]; //last element of list
+            nums.RemoveAll(x => x == last); //removing all items that equals the last element
+            Console.WriteLine(string.Join(" ",nums));
+        }
+    }
+}
